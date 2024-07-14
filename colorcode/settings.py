e8cf5f19e846d6f4454686996837ce6337b8717b
Shauna Hathaway McHugh <shaunadoo97@gmail.com>
2024-07-14 15:40:52 +0000

    Trying to add Database

--- colorcode/settings.py
+++ colorcode/settings.py
@@ -11,6 +11,10 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import os
+import dj_database_url
+if os.path.isfile('env.py'):
+    import env
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -23,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-b2$4%7v+=yx%qxn2ono(1_hd*tve7$ahjdp#+ii(v6^c9n)%11'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = [ '8000-shaunadoo97-artistblogp-ub6hfh1w6sh.ws.codeinstitute-ide.net', '.herokuapp.com']
 
@@ -74,12 +78,12 @@ WSGI_APPLICATION = 'colorcode.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+# DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': BASE_DIR / 'db.sqlite3',
+#    }
+# }
 
 
 # Password validation
